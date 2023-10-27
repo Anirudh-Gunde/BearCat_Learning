ec6504eacb62d691e8f471df66599f25e6ab8e83
Anirudh Gunde <anirudh.gunde1@gmail.com>
2023-10-27 16:19:18 -0500

    Added firebase connection code in MainActivity.java,added dependencies for firebase connection in gradles.

--- BearCatLearning/app/src/main/java/com/example/bearcatlearning/MainActivity.java
+++ BearCatLearning/app/src/main/java/com/example/bearcatlearning/MainActivity.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextUserID, editTextPassword;
     private Button buttonLogin;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,47 +27,39 @@ public class MainActivity extends AppCompatActivity {
         editTextUserID = findViewById(R.id.student_id_edittext);
         editTextPassword = findViewById(R.id.password_edittext);
         buttonLogin = findViewById(R.id.login_button);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String userID = editTextUserID.getText().toString();
                 String password = editTextPassword.getText().toString();
+                firebaseAuth.signInWithEmailAndPassword(userID, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Check if user exists in the Realtime Database
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login successful!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
 
-
-                if (isStudent(userID, password)) {
-
-                    Intent intent = new Intent(MainActivity.this, dashboardActivity.class);
-                    intent.putExtra("studentID",userID);
-                    startActivity(intent);
-                } else if (isAdmin(userID, password)) {
-
-                    Intent intent = new Intent(MainActivity.this, AdminDashboardActivity.class);
-                    startActivity(intent);
-                } else {
-                    // Invalid credentials, show error message
-                    // ...
-                }
+                                    // If sign-in is successful, navigate to the dashboard activity
+                                    Intent intent = new Intent(MainActivity.this, dashboardActivity.class);
+                                    startActivity(intent);
+                                    finish(); // Close the current activity to prevent the user from going back
+                                } else {
+                                    // Sign-in failed
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login failed!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
+                                    // Handle failed login
+                                }
+                            }
+                        });
             }
         });
     }
-
-    private boolean isStudent(String userID, String password) {
-        if(userID.equals("Student") && password.equals("123456"))
-        {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean isAdmin(String userID, String password) {
-
-        if(userID.equals("Professor") && password.equals("123456"))
-        {
-            return true;
-        }
-        return false;
-    }
-
-
 }
