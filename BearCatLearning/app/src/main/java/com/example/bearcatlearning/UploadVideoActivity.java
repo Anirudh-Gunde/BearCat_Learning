66688f7d22f3217ab168c2530e4bc69bde06bcd7
Manasa Akula <s560998@nwmissouri.edu>
2023-10-27 20:23:15 -0500

    Implemented the methods to uploading and storing the videos to firebasedatabase(ManasaAkula-S560998)

--- BearCatLearning/app/src/main/java/com/example/bearcatlearning/UploadVideoActivity.java
+++ BearCatLearning/app/src/main/java/com/example/bearcatlearning/UploadVideoActivity.java
@@ -36,5 +36,79 @@ public class UploadVideoActivity extends Activity{
         videoIntent.setType("video/*"); // Allow only video files
         startActivityForResult(videoIntent, VIDEO_UPLOAD_REQUEST_CODE);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == VIDEO_UPLOAD_REQUEST_CODE && resultCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+
+            if (videoUri != null) {
+                uploadVideoToFirebaseStorage(videoUri);
+            } else {
+                showToast("Video selection failed");
+            }
+            finish();
+        } else {
+            showToast("Video selection canceled");
+            finish();
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void uploadVideoToFirebaseStorage(Uri videoUri) {
+        // Create a unique key for the video
+        String videoId = databaseReference.push().getKey();
+        // Define the StorageReference for the video in Firebase Storage
+        StorageReference videoRef = storageReference.child(videoId);
+        try
+        {
+            InputStream inputStream = getContentResolver().openInputStream(videoUri);
+
+            UploadTask uploadTask = videoRef.putStream(inputStream);
+
+            uploadTask.addOnSuccessListener(taskSnapshot -> {
+                // Video uploaded successfully, get the download URL
+                videoRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                    String downloadUrl = uri.toString();
+                    storeVideoMetadataInDatabase(videoId, downloadUrl);
+                });
+            }).addOnFailureListener(e -> showToast("Video upload failed: " + e.getMessage()));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            showToast("Failed to read video: " + e.getMessage());
+        }
+    }
+
+    private void storeVideoMetadataInDatabase(String videoId, String downloadUrl) {
+        // Create a data object to represent the video metadata
+        VideoMetadata videoMetadata = new VideoMetadata(downloadUrl);
+
+        // Store the metadata in Firebase Realtime Database
+        databaseReference.child(videoId).setValue(videoMetadata)
+                .addOnSuccessListener(aVoid -> showToast("Video metadata stored in Firebase Database"))
+                .addOnFailureListener(e -> showToast("Failed to store video metadata: " + e.getMessage()));
+    }
+
+    private class VideoMetadata {
+        private String downloadUrl;
+
+        public VideoMetadata()
+        {
+            // Default constructor required for Firebase Realtime Database
+        }
+
+        public VideoMetadata(String downloadUrl) {
+            this.downloadUrl = downloadUrl;
+        }
+
+        public String getDownloadUrl() {
+            return downloadUrl;
+        }
+    }
 }
