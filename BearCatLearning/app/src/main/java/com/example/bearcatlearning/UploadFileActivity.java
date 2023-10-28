88ad150c60df0e9f1c85b56f0602d6f45d9c2e8d
Manasa Akula <s560998@nwmissouri.edu>
2023-10-27 19:54:46 -0500

    Implemented the methods to uploading and Storing the file to firedatabase(Manasa Akula-S560998)

--- BearCatLearning/app/src/main/java/com/example/bearcatlearning/UploadFileActivity.java
+++ BearCatLearning/app/src/main/java/com/example/bearcatlearning/UploadFileActivity.java
@@ -39,4 +39,76 @@ public class UploadFileActivity extends Activity{
         startActivityForResult(fileIntent, FILE_UPLOAD_REQUEST_CODE);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == FILE_UPLOAD_REQUEST_CODE && resultCode == RESULT_OK) {
+            Uri fileUri = data.getData();
+
+            if (fileUri != null) {
+                uploadFileToFirebaseStorage(fileUri);
+            } else {
+                showToast("File selection failed");
+            }
+            finish();
+        } else {
+            showToast("File selection canceled");
+            finish();
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void uploadFileToFirebaseStorage(Uri fileUri) {
+        // Create a unique key for the file
+        String fileId = databaseReference.push().getKey();
+
+        // Define the StorageReference for the file in Firebase Storage
+        StorageReference fileRef = storageReference.child(fileId);
+
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(fileUri);
+
+            UploadTask uploadTask = fileRef.putStream(inputStream);
+
+            uploadTask.addOnSuccessListener(taskSnapshot -> {
+                // File uploaded successfully, get the download URL
+                fileRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                    String downloadUrl = uri.toString();
+                    storeFileMetadataInDatabase(fileId, downloadUrl);
+                });
+            }).addOnFailureListener(e -> showToast("File upload failed: " + e.getMessage()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            showToast("Failed to read file: " + e.getMessage());
+        }
+    }
+    private void storeFileMetadataInDatabase(String fileId, String downloadUrl) {
+        // Create a data object to represent the file metadata
+        FileMetadata fileMetadata = new FileMetadata(downloadUrl);
+
+        // Store the metadata in Firebase Realtime Database
+        databaseReference.child(fileId).setValue(fileMetadata)
+                .addOnSuccessListener(aVoid -> showToast("File metadata stored in Firebase Database"))
+                .addOnFailureListener(e -> showToast("Failed to store file metadata: " + e.getMessage()));
+    }
+
+    private class FileMetadata {
+        private String downloadUrl;
+
+        public FileMetadata() {
+            // Default constructor required for Firebase Realtime Database
+        }
+
+        public FileMetadata(String downloadUrl) {
+            this.downloadUrl = downloadUrl;
+        }
+
+        public String getDownloadUrl() {
+            return downloadUrl;
+        }
+    }
 }
