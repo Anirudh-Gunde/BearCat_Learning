39a86680a14a9fd36052a91fca79b55cd87f624b
Anirudh Gunde <anirudh.gunde1@gmail.com>
2023-11-15 23:52:44 -0600

    Updated contentactivity.java file for viewing course videos

--- BearCat_Learning/BearCatLearning/app/src/main/java/com/example/bearcatlearning/contentActivity.java
+++ BearCat_Learning/BearCatLearning/app/src/main/java/com/example/bearcatlearning/contentActivity.java
@@ -1,34 +1,79 @@
 package com.example.bearcatlearning;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
 import android.widget.MediaController;
+import android.widget.ProgressBar;
 import android.widget.VideoView;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.ArrayList;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class contentActivity extends AppCompatActivity {
 
     private VideoView videoView;
-    private Button playButton, pauseButton, stopButton;
-    private ListView materialsListView;
+    private ProgressBar progressBar;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+    private MediaPlayer mediaPlayer;
+    private MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
 
-        // Initialize UI elements
         videoView = findViewById(R.id.video_view);
-        playButton = findViewById(R.id.play_button);
-        pauseButton = findViewById(R.id.pause_button);
-        stopButton = findViewById(R.id.stop_button);
-        materialsListView = findViewById(R.id.materials_list);
+        progressBar = findViewById(R.id.progress_bar);
 
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference().child("Parkway.mp4"); // Replace with your video file name
 
+        initializeMediaPlayer();
+
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                videoView.setVideoURI(uri);
+                videoView.requestFocus();
+                videoView.start();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void initializeMediaPlayer() {
+        mediaPlayer = new MediaPlayer();
+        mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        videoView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+            @Override
+            public boolean onInfo(MediaPlayer mediaPlayer, int what, int extra) {
+                if (what == MediaPlayer.MEDIA_INFO_BUFFERING_START) {
+                    progressBar.setVisibility(View.VISIBLE);
+                } else if (what == MediaPlayer.MEDIA_INFO_BUFFERING_END) {
+                    progressBar.setVisibility(View.GONE);
+                }
+                return false;
+            }
+        });
     }
 }
-
