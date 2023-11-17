ef8319aed4b7c8339eddcfdb36aaa25b8fd0cf42
JoyceLingam <joycemeyerlingam03@gmail.com>
2023-11-17 02:11:18 -0600

    added logout button to video view layout contentActivity.xml

--- BearCat_Learning/BearCatLearning/app/src/main/java/com/example/bearcatlearning/contentActivity.java
+++ BearCat_Learning/BearCatLearning/app/src/main/java/com/example/bearcatlearning/contentActivity.java
@@ -1,9 +1,12 @@
 package com.example.bearcatlearning;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.VideoView;
@@ -11,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -22,7 +26,9 @@ public class contentActivity extends AppCompatActivity {
     private StorageReference storageReference;
     private MediaPlayer mediaPlayer;
     private MediaController mediaController;
+    private ImageButton i;
 
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +36,7 @@ public class contentActivity extends AppCompatActivity {
 
         videoView = findViewById(R.id.video_view);
         progressBar = findViewById(R.id.progress_bar);
+        i = findViewById(R.id.logout);
 
         firebaseStorage = FirebaseStorage.getInstance();
         storageReference = firebaseStorage.getReference().child("Parkway.mp4"); // Replace with your video file name
@@ -56,6 +63,21 @@ public class contentActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.GONE);
             }
         });
+        i.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Sign out the user from Firebase Authentication
+                FirebaseAuth.getInstance().signOut();
+
+                // Navigate back to the login screen (MainActivity)
+                Intent intent = new Intent(contentActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish(); // Close the current activity to prevent the user from going back
+
+            }
+        });
+
     }
 
     private void initializeMediaPlayer() {
@@ -75,5 +97,6 @@ public class contentActivity extends AppCompatActivity {
                 return false;
             }
         });
+
     }
 }
